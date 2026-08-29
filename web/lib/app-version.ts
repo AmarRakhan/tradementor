c6fa631c4db1065b92deb7166d8b13b233f4ed44
AmarRakhan <amar_rakhan@hotmail.com>
2026-08-29 14:31:13 +0200

    chore: release webapp V46

--- web/lib/app-version.ts
+++ web/lib/app-version.ts
@@ -1,4 +1,4 @@
-export const WEBAPP_VERSION = "45";
+export const WEBAPP_VERSION = "46";
 export function webappVersionLabel(buildNumber: string) {
   return `Webapp versie ${WEBAPP_VERSION} · build ${buildNumber}`;
 }
