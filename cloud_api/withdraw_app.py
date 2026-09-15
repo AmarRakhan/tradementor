1050e5d15b2d8a2a521bbd2e53b1025a2de8243a
AmarRakhan <amar_rakhan@hotmail.com>
2026-09-15 19:30:18 +0200

    feat: register portfolio emergency hedge routes and worker

--- cloud_api/withdraw_app.py
+++ cloud_api/withdraw_app.py
@@ -2,6 +2,13 @@
 import main
 import withdraw_extension  # noqa: F401  - importing registers routes on main.app
 from snapshot_profit_close_extension import router as snapshot_profit_close_router
+from aster_portfolio_emergency_hedge import install as install_aster_portfolio_emergency_hedge
 
 main.app.include_router(snapshot_profit_close_router)
+install_aster_portfolio_emergency_hedge(
+    main.app,
+    db=main.db,
+    load_secret=main.load_aster_secret,
+    auth_app=main.auth_app,
+)
 app = main.app
