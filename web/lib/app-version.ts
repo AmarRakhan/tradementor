862cc8c1dc43281e2b5536c36d7dff27581b6b2f
AmarRakhan <amar_rakhan@hotmail.com>
2026-09-21 00:45:18 +0200

    V46 build 384: release Portfolio Cycle snapshot

--- web/lib/app-version.ts
+++ web/lib/app-version.ts
@@ -1,5 +1,5 @@
 export const WEBAPP_VERSION = "46";
-export const WEBAPP_BUILD_NUMBER = "383";
+export const WEBAPP_BUILD_NUMBER = "384";
 
 export function webappVersionLabel(buildNumber: string = WEBAPP_BUILD_NUMBER) {
   return `Webapp versie ${WEBAPP_VERSION} · build ${buildNumber}`;
